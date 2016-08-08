2f758d8963b4ea6d93343aaba57d9228d1b4bd76
Yilmazgithub <mohamedyilmaz98@gmail.com>
2016-08-09 00:29:06 +0530

    Add files via upload

---
+++ DateTest.java
@@ -0,0 +1,24 @@
+package DateandTime;
+import java.util.*;
+public class DateTest {
+	
+     public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		Date a = new Date();
+		System.out.println("Enter the day: ");
+		int date = input.nextInt();
+		System.out.println("Enter the month: ");
+		int month = input.nextInt();
+		System.out.println("Enter the year: ");
+		int year = input.nextInt();
+		input.close();
+		a.setDay(date);
+		a.setMonth(month);
+		a.setYear(year);
+		
+		a.displayDate();
+		System.out.println("Program done by Mohamed Yilmaz Ibrahim(Reg No:2015103064)");
+
+	}
+}
